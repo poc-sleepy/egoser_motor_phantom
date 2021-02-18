5657b069efd752a081fd34f90d058c5d676e661a
poc-sleepy <mattari.honey.latte@gmail.com>
2021-02-18 22:56:13 +0900

    settings_template.py 新規作成
    settings_template.py
    ・settings.pyのテンプレになるよう、新規作成（settingsの項目が増えてきたので）

---
+++ settings_template.py
@@ -0,0 +1,25 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+  Twitter
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+TWITTER_KEY = ''  # Your Twitter Bearer
+TWEET_QUERY = 'from:TwitterDev -is:retweet'
+MAX_RESULTS = 10  # minimum: 10
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+  Slack
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+SLACK_TOKEN = ''  # Your Slack token
+CHANNEL_TO_POST = '#random'
+TWEET_POST_COLOR = '#1976d2'  # #XXXXXX or None
+RETWEET_POST_COLOR = '#4caf50'  # #XXXXXX or None
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+  File Path
+  When you use this app in crontab,
+  1. Set ABSOLUTE path to each path.
+  2. Set True to PICKLE_PATH_IS_ABSOLUTE.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+TWEET_FILE_PATH = 'tweets.jsonl'
+LOG_FILE_PATH = 'execute.log'
+PICKLE_FILE_PATH = 'newest_id.pickle'
+PICKLE_PATH_IS_ABSOLUTE = False
